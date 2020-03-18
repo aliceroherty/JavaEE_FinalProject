69c99b8efc4d9661f2ca1fa90470d6ef105b122a
Aldirus <soakes68@hotmail.ca>
2020-03-18 20:41:13 -0300

    Updated EmployeeController/view

--- App/com.ats.webapplication/src/java/com/ats/controllers/EmployeesController.java
+++ App/com.ats.webapplication/src/java/com/ats/controllers/EmployeesController.java
@@ -3,8 +3,10 @@ package com.ats.controllers;
 import com.ats.models.EmployeeFactory;
 import com.ats.models.IEmployee;
 import com.ats.service.IEmployeeService;
+import com.ats.service.ITaskService;
 import com.ats.service.ServiceFactory;
 import com.ats.viewmodels.EmployeeListViewModel;
+import com.ats.viewmodels.TaskListViewModel;
 import java.io.IOException;
 import java.util.Date;
 import javax.servlet.ServletException;
@@ -13,29 +15,32 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Employees Controller
+ *
  * @author Alice Roherty-Carrier
  * @date 03-04-2020
  */
 public class EmployeesController extends CommonController {
+
     private IEmployeeService service = ServiceFactory.createEmployeeInstance();
-    
+
     private static final String EMPLOYEES = "/employees.jsp";
     private static final String CREATE_EMPLOYEE = "/createEmployee.jsp";
-    
+    private static final String ADD_EMPLOYEESKILLS = "/addEmployeeSkils.jsp";
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getRequestURI();
-        
+
         if (path != null) {
             String[] pathParts = path.split("/");
             String action = pathParts[(pathParts.length - 1)];
-            
+
             switch (action) {
                 case "employees":
                     EmployeeListViewModel vm = new EmployeeListViewModel();
-                    
+
                     vm.setEmployees(service.getEmployees());
-                    
+
                     request.setAttribute("vm", vm);
                     super.setView(request, EMPLOYEES);
                     super.getView().forward(request, response);
@@ -43,17 +48,27 @@ public class EmployeesController extends CommonController {
                 case "createEmployee":
                     super.setView(request, CREATE_EMPLOYEE);
                     break;
+                case "addEmployeeSkills":
+                    TaskListViewModel tlvm = new TaskListViewModel();
+                    ITaskService taskService = ServiceFactory.createTaskInstance();
+
+                    tlvm.setTasks(taskService.getTasks());
+                    request.setAttribute("tlvm", tlvm);
+
+                    super.setView(request, ADD_EMPLOYEESKILLS);
+
+                    super.getView().forward(request, response);
             }
         }
-        
+
         super.getView().forward(request, response);
     }
-    
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             String path = request.getRequestURI();
-        
+
             if (path != null) {
                 String[] pathParts = path.split("/");
                 String action = pathParts[(pathParts.length - 1)];
@@ -61,21 +76,21 @@ public class EmployeesController extends CommonController {
                 switch (action) {
                     case "employees":
                         EmployeeListViewModel vm = new EmployeeListViewModel();
-                        
+
                         String searchText = getValue(request, "search");
-                        
+
                         if (!searchText.equals("")) {
                             vm.setEmployees(service.searchEmployees(searchText));
                         } else {
                             vm.setEmployees(service.getEmployees());
                         }
-                        
+
                         request.setAttribute("vm", vm);
-                        
+
                         if (vm.getEmployees().isEmpty()) {
                             request.setAttribute("message", "No Results");
                         }
-                        
+
                         super.setView(request, EMPLOYEES);
                         super.getView().forward(request, response);
                         break;
@@ -86,7 +101,7 @@ public class EmployeesController extends CommonController {
                         if (!service.isValid(employee)) {
                             request.setAttribute("errors", employee.getErrors());
                         }
-                        
+
                         if (employee.getId() > 0) {
                             request.setAttribute("message", "Employee Created Successfully");
                         }
@@ -100,7 +115,7 @@ public class EmployeesController extends CommonController {
                         if (id != 0) {
                             rowsAffected = service.deleteEmployee(id);
                         }
-                        
+
                         System.out.println(rowsAffected);
                         break;
                 }
@@ -109,7 +124,7 @@ public class EmployeesController extends CommonController {
             System.out.println(e.getMessage());
         }
     }
-    
+
     private IEmployee setEmployee(HttpServletRequest request) {
         String firstName = super.getValue(request, "firstName");
         String lastName = super.getValue(request, "lastName");
